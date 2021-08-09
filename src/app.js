2df6d81df54deec8096f9a50438f34facece0855
paxonpresley <76454250+paxonpresley@users.noreply.github.com>
2021-08-09 13:16:48 -0500

    basic functions down

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@
 window.onload = function () {
     document.querySelector('.preloader').style.display = "none";
 };
-
+// date
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
+// defaoult text
+let inputItem = document.getElementById('inputItem');
+const dumby = inputItem.getAttribute('value')
