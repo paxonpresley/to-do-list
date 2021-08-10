ac0e155545141eb2ed9faecd86d0a47045dc4166
paxonpresley <76454250+paxonpresley@users.noreply.github.com>
2021-08-09 22:30:49 -0500

    function forEach kinda done :/

--- src/app.js
+++ src/app.js
@@ -7,4 +7,9 @@ const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
 
 // checked btn
-const doneBtns = document.querySelectorAll('done-btn');
+const doneBtns = document.querySelectorAll('.done-btn');
+doneBtns.forEach(function(doneBtn){
+   doneBtn.addEventListener('click', function(){
+    //    console.log('penis');
+   });
+});
