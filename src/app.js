d65c36bc2711c595642f43efff04c367c72f0cd0
paxonpresley <76454250+paxonpresley@users.noreply.github.com>
2021-08-09 23:12:26 -0500

    i made it check off

--- src/app.js
+++ src/app.js
@@ -9,7 +9,9 @@ date.innerHTML = new Date().getFullYear();
 // checked btn
 const doneBtns = document.querySelectorAll('.done-btn');
 doneBtns.forEach(function(doneBtn){
-   doneBtn.addEventListener('click', function(){
-    //    console.log('penis');
+   doneBtn.addEventListener('click', function(e){
+    //console.log(e.currentTarget.parentElement.parentElement);
+    const listItem = e.currentTarget.parentElement.parentElement;
+    listItem.classList.toggle('checked');
    });
 });
